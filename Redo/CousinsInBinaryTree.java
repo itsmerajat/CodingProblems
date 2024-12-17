1566abfcb00cbc42b54ae860082a34f98f0081ca
itsmerajat <114578716+itsmerajat@users.noreply.github.com>
2024-12-17 17:21:20 +0530

    Day 20

--- Redo/CousinsInBinaryTree.java
+++ Redo/CousinsInBinaryTree.java
@@ -1,6 +1,6 @@
 package Redo;
 
-import Redo.TreeNode;
+import Redo.helper.TreeNode;
 
 public class CousinsInBinaryTree {
 
---
+++ Redo/helper/ReverseLinkedList.java
@@ -0,0 +1,20 @@
+package Redo.helper;
+
+public class ReverseLinkedList {
+
+    public static ListNode reverse(ListNode head){
+
+        ListNode prev = null;
+        ListNode curr = head;
+        
+        while(curr!=null){
+            ListNode next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+        return prev;
+
+    }
+    
+}
